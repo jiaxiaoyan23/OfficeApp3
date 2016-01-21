5e028690f674376c0fe272240c27cfd02ddfd6c0
JingyuShao <shaojingyu.zju@gmail.com>
2016-01-20 16:40:29 -0800

    css update for the run code button wrapper

--- OfficeApp3Web/excel-snippets/setValueInRange.js
+++ OfficeApp3Web/excel-snippets/setValueInRange.js
@@ -2,7 +2,9 @@
 Excel.run(function (ctx) {
     var activeWorksheet = ctx.workbook.worksheets.getActiveWorksheet();
 	activeWorksheet.getRange("A1:C3").values = 7;
-	return ctx.sync();
+	return ctx.sync().then(function () {
+	    console.log("Success! Set single value in range A1:C3.");
+	});
 }).catch(function (error) {
 	console.log(error);
 });
