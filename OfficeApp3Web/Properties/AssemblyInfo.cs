09367f9958ab6a708e85b6685ab907f4deecdecf
JingyuShao <shaojingyu.zju@gmail.com>
2016-02-29 12:37:09 -0800

    run button space update

--- OfficeApp3Web/Properties/AssemblyInfo.cs
+++ OfficeApp3Web/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("OfficeApp3Web")]
+[assembly: AssemblyTitle("APITutorialWeb")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("OfficeApp3Web")]
-[assembly: AssemblyCopyright("Copyright ©  2015")]
+[assembly: AssemblyProduct("APITutorialWeb")]
+[assembly: AssemblyCopyright("Copyright ©  2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
